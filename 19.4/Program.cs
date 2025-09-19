338e8dea34299f1d7b159a956ba2acd983a0694d
giupe <giupe@MSI>
2025-09-19 17:31:20 +0200

    Add project files.

---
+++ 19.4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _19._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomi = new List<string> { "Mike", "Bob", "Charlie", "Luck", "Emily", "Frank" };
+            int n;
+            do
+            {
+                Console.WriteLine("inserici il numero della posizione (non deve essere < 0 || > 6)");
+
+
+            }
+            while (!int.TryParse(Console.ReadLine(), out n) || n < 0 || n > 6);
+
+            Console.WriteLine(nomi[n - 1]);
+        }
+    }
+}
